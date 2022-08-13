f8447af33baf975392712f8f68299784ac77252c
Алексей Ипполитов <setgecko@gmail.com>
2022-08-13 14:03:54 +0300

    Добавьте файлы проекта.

---
+++ FirstApplication.ConsoleApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstApplication.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Укажите ваше имя?");
+            string name = Console.ReadLine();
+            Console.WriteLine("Укажите вашу фамилию?");
+            string surname = Console.ReadLine();
+            Console.WriteLine("Откуда вы?");
+            string city = Console.ReadLine();
+            Console.WriteLine("Дрогой(-ая), {0} {1} из {2}, желаю вам хорошего дня и настроения!", surname, name, city);
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SecondApplication/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecondApplication
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Как Вас зовут?");
+            string name = Console.ReadLine();
+            Console.WriteLine("Сколько Вам лет?");
+            string age = Console.ReadLine();
+            Greet(name, age);
+            Console.WriteLine("Рад был познакомиться!");
+            Console.ReadLine();
+        }
+        static void Greet(string name, string age)
+        {
+            Console.WriteLine("Здравствуйте, {0}", name);
+            Console.WriteLine("Мне сообщили, что Вам {0}", age);
+        }
+    }
+}
